8e391937f1542affcab9ac91829ec205b993dd94
Galih Dwi <me.oceaner@gmail.com>
2026-06-24 20:32:48 +0700

    Clear git cache

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/env.php';
-
-return [
-    'class' => \yii\mongodb\Connection::class,
-    'dsn' => app_env('MONGODB_DSN', 'mongodb://localhost:27017/lapakpay'),
-    'defaultDatabaseName' => app_env('MONGODB_DATABASE', 'lapakpay'),
-];
